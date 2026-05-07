7205e74d5afb75d29d0317fd6a1f62fb2b5b9bdd
Djordje Stefanovic <djstefanovic@tenstorrent.com>
2026-05-07 09:38:17 +0000

    Fix tests.

--- lib/Target/TTNN/TTNNToFlatbuffer.cpp
+++ lib/Target/TTNN/TTNNToFlatbuffer.cpp
@@ -1986,7 +1986,7 @@ createEltwiseBinaryCompositeWithoutFusedActivationOp(
 
   auto result = op.getResult();
 
-  auto memoryConfig = getMemoryConfigIfNeeded(cache, op);
+  auto memoryConfig = toFlatbuffer(cache, op.getMemoryConfigAttr());
 
   auto out = cache.getOrCreateNoSharding(result, tensorValueToFlatbuffer,
 
