332e6c1572ab919726940f05c54ad12f2fa1ea24
ttdloke <dloke@tenstorrent.com>
2026-05-23 13:29:50 +0000

    block factor wip

--- lib/Dialect/D2M/Analysis/BlockFactorAnalysis.cpp
+++ lib/Dialect/D2M/Analysis/BlockFactorAnalysis.cpp
@@ -126,6 +126,12 @@ classifyAutoSearch(GenericOp genericOp,
   if (reductionDims.empty()) {
     SmallVector<std::size_t> candidateDims =
         getParticipatingDimsWithShardFactors(genericOp, shardFactors);
+    for (std::size_t reductionDim : reductionDims) {
+      if (shardFactors[reductionDim] > 1 &&
+          !llvm::is_contained(candidateDims, reductionDim)) {
+        candidateDims.push_back(reductionDim);
+      }
+    }
     if (candidateDims.empty()) {
       return std::nullopt;
     }
